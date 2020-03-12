2fba584c5c85e0918bd51abb5473595b9116f229
palma001 <palmaluis1997@gmail.com>
2020-03-12 13:52:29 -0400

    vue calend

--- src/views/ScheduleTeacher.vue
+++ src/views/ScheduleTeacher.vue
@@ -1,109 +1,102 @@
 <template>
   <v-layout>
-    <v-flex>
-      <v-sheet height="750">
-        <!-- now is normally calculated by itself, but to keep the calendar in this date range to view events -->
-        <v-calendar
-          ref="calendar"
-          :now="today"
-          :value="today"
-          color="primary"
-          type="week"
-        >
-          <!-- the events at the top (all-day) -->
-          <template v-slot:dayHeader="{ date }">
-            <template v-for="event in eventsMap[date]">
-              <!-- all day events don't have time -->
-              <div
-                v-if="!event.time"
-                :key="event.title"
-                class="my-event"
-                @click="open(event)"
-                v-html="event.title"
-              ></div>
-            </template>
-          </template>
-          <!-- the events at the bottom (timed) -->
-          <template v-slot:dayBody="{ date, timeToY, minutesToPixels }">
-            <template v-for="event in eventsMap[date]">
-              <!-- timed events -->
-              <div
-                v-if="event.time"
-                :key="event.title"
-                :style="{ top: timeToY(event.time) + 'px', height: minutesToPixels(event.duration) + 'px' }"
-                class="my-event with-time"
-                @click="open(event)"
-                v-html="event.title"
-              ></div>
-            </template>
-          </template>
-        </v-calendar>
-      </v-sheet>
-    </v-flex>
+    <vue-cal selected-date="2018-11-19"
+      :time-from="7 * 60"
+      :time-to="22 * 60"
+      :disable-views="['years', 'year', 'month']"
+      :events="events"
+      :on-event-click="onEventClick">
+    </vue-cal>
+
+    <!-- Using Vuetify -->
+    <v-dialog v-model="showDialog">
+      <v-card>
+        <v-card-title>
+          <v-icon>{{ selectedEvent.icon }}</v-icon>
+          <h1>{{ selectedEvent.title }}</h1>
+          <v-spacer/>
+          <strong>{{ selectedEvent.startDate && selectedEvent.startDate.format('DD/MM/YYYY') }}</strong>
+        </v-card-title>
+        <v-card-text>
+          <p v-html="selectedEvent.contentFull"/>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 <script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
 export default {
+  components: { VueCal },
   data: () => ({
-    today: '2019-01-08',
-    events: [
-      {
-        title: 'Weekly Meeting',
-        date: '2019-01-07',
-        time: '09:00',
-        duration: 45
-      },
-      {
-        title: 'Thomas\' Birthday',
-        date: '2019-01-10'
-      },
-      {
-        title: 'Mash Potatoes',
-        date: '2019-01-09',
-        time: '12:00',
-        duration: 180
-      }
-    ]
+    selectedEvent: {},
+    showDialog: false,
+    events: []
   }),
-  computed: {
-    // convert the list of events into a map of lists keyed by date
-    eventsMap () {
-      const map = {}
-      this.events.forEach(e => (map[e.date] = map[e.date] || []).push(e))
-      return map
-    }
-  },
-  mounted () {
-    this.$refs.calendar.scrollToTime('08:00')
+  created () {
+    this.getHorarios()
   },
   methods: {
-    open (event) {
-      alert(event.title)
+    async getHorarios () {
+      let { res } = await this.$services.getData(['ficde', 'semestres_materias'], {
+        paginate: true,
+        perPage: 1
+      })
+      this.getInfo(res.data.data)
+    },
+    getInfo (data) {
+      this.events = data.map(element => {
+        return {
+          start: `2018-11-22 ${element.hora_e}`,
+          end: `2018-11-22 ${element.hora_s}`,
+          title: `${element.nombreMateria} - ${element.nombreProfesor}`,
+          contentFull: `
+            Nombre del Aula - <strong>${element.nombreAula}</strong>
+            <br>
+            <ul>
+              <li>Nombre del Profesor: ${element.nombreProfesor}</li>
+            </ul>
+          `,
+          class: 'sport'
+        }
+      })
+      console.log(this.events)
+    },
+    onEventClick (event, e) {
+      this.selectedEvent = event
+      this.showDialog = true
+
+      // Prevent navigating to narrower view (default vue-cal behavior).
+      e.stopPropagation()
     }
   }
 }
 </script>
-<style lang="stylus" scoped>
-  .my-event {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    border-radius: 2px;
-    background-color: #1867c0;
-    color: #ffffff;
-    border: 1px solid #1867c0;
-    font-size: 12px;
-    padding: 3px;
-    cursor: pointer;
-    margin-bottom: 1px;
-    left: 4px;
-    margin-right: 8px;
-    position: relative;
+<style>
+.vuecal__event {
+  cursor: pointer;
+}
 
-    &.with-time {
-      position: absolute;
-      right: 4px;
-      margin-right: 0px;
-    }
-  }
+.vuecal__event-title {
+  font-size: 1.2em;
+  font-weight: bold;
+  margin: 4px 0 8px;
+}
+
+.vuecal__event-time {
+  display: inline-block;
+  margin-bottom: 12px;
+  padding-bottom: 12px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+}
+.vuecal__flex .weekday-label span:last-child{
+  display: none;
+}
+.vuecal__title-bar {
+  display: none;
+}
+.vuecal__event-content {
+  font-style: italic;
+}
 </style>
